eb98bbcfa614841ca1d85292c9d67cc816313643
esaupe <esaupe@ocient.com>
2020-08-03 15:28:15 +0000

    bumb db version

--- src/main/java/com/ocient/jdbc/JDBCDriver.java
+++ src/main/java/com/ocient/jdbc/JDBCDriver.java
@@ -22,7 +22,7 @@ import java.util.logging.Level;
 public class JDBCDriver implements Driver
 {
 
-	private static String version = "7.0.0";
+	private static String version = "7.0.1";
 	private static final Logger LOGGER = Logger.getLogger( "com.ocient.jdbc" );
 	private static String logFileName;
 	private static FileHandler logHandler;
